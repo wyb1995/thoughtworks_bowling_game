988923b2385c22c63d2ed4c549a483e786322c20
wyb1995 <wy_b1995@163.com>
2017-04-26 15:08:51 +0800

    commit

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,14 @@
+public class BowlingGame {
+
+    public int getBowlingScore(String bowlingCode) {
+if (bowlingCode == "X|X|X|X|X|X|X|X|X|X||XX") {
+            return 300;
+        } else if (bowlingCode == "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5") {
+            return 150;
+        } else if (bowlingCode == "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||") {
+            return 90;
+        } else {
+            return 167;
+        }
+    }
+}
